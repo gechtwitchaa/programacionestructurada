df2ca004a5606d4702f54eb6ea6aaea8f2d25262
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-12 11:51:05 +0100

    .

--- 1b.cpp
+++ 1b.cpp
@@ -10,8 +10,14 @@ struct Estudiante {
 int main() {
 
     struct Estudiante estudiante1;
-    
+
     snprintf(estudiante1.nombre, sizeof(estudiante1.nombre), "%s", "Juan");
     estudiante1.edad = 20;
     estudiante1.promedio = 9.5;
+    
+    printf("Nombre: %s\n", estudiante1.nombre);
+    printf("Edad: %d\n", estudiante1.edad);
+    printf("Promedio: %.2f\n", estudiante1.promedio);
 
+    return 0;
+}
