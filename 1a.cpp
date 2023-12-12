ab0bb062c179d8e74e3b7fab5175ae6a534bf0d8
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-12 11:44:35 +0100

    .

--- 1a.cpp
+++ 1a.cpp
@@ -7,3 +7,11 @@ struct Estudiante {
     float promedio;
 };
 
+int main() {
+
+    struct Estudiante estudiante1;
+
+    strcpy(estudiante1.nombre, "Juan");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 8.5;
+
